636f378380021d22eab6af96759da32bc2820942
Sachin Mahajan <sachinm78@gmail.com>
2019-03-25 16:09:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name
 
-  
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -14,11 +14,18 @@ class Artist
     @@song_count +=1
   end
 
-  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
 
   def songs
     @songs 
   end
 
-  
+  def self.song_count
+    @@song_count
+  end
 end
